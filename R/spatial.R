175fe5f20d619a195b2f4e23e7a890a9806d0eba
Robert+1-at-925791393128 <r.hijmans@gmail.com>
2020-03-30 18:29:31 -0700

    a

--- R/spatial.R
+++ R/spatial.R
@@ -7,11 +7,11 @@ function(x, ..., filename="", overwrite=FALSE, wopt=list())  {
 	preds <- list(...)
 	if (length(preds) == 0) stop("no predictor variables")
 	
-	e <- sapply(preds, function(i) as.vector(ext(i)))
+	e <- sapply(preds, function(i) as.vector(terra::ext(i)))
 	if (!all(apply(e == e[,1], 1, all))) {
 		stop("extents are not equal")
 	}
-	r <- sapply(preds, res)
+	r <- sapply(preds, terra::res)
 	if (!(all(apply(r == r[,1], 1, all)))) {
 		stop("resolutions are not equal")	
 	}
@@ -20,7 +20,7 @@ function(x, ..., filename="", overwrite=FALSE, wopt=list())  {
 	if (is.null(nms) | any(nms=="")) stop ("SpatRaster arguments must be named")
 	# check if nms in model
 
-	nlyrs <- sapply(preds, nlyr)
+	nlyrs <- sapply(preds, terra::nlyr)
 	if (!all(nlyrs %in% c(1, 12))) stop("all SpatRaster objects must have either 1 or 12 layers")
 	di <- nlyrs == 12
 	dpreds <- preds[di]
@@ -29,15 +29,15 @@ function(x, ..., filename="", overwrite=FALSE, wopt=list())  {
 	out <- rast(preds[[1]])
 	nlyr(out) <- ifelse(x$max, 1, 12)
 	nc <- ncol(out)
-	lapply(preds, readStart)
+	lapply(preds, terra::readStart)
 	
-	b <- writeStart(out, filename, overwrite, wopt=list(memfrac=0.1))
+	b <- terra::writeStart(out, filename, overwrite, wopt=list(memfrac=0.1))
 	for (i in 1:b$n) {
-		z <- sapply(nms, function(name) { removePredictor(x, name)})
+		removePredictor(x, "ALL")
 		
 		if (length(dpreds) > 0) {
 			v <- lapply(dpreds, function(r) {
-				as.vector(t(readValues(r, b$row[i], b$nrows[i], 1, nc, TRUE)))
+				as.vector(t(terra::readValues(r, b$row[i], b$nrows[i], 1, nc, TRUE)))
 			})
 			vv <- do.call(cbind, v)
 			names(vv) <- nms[di]
@@ -45,7 +45,7 @@ function(x, ..., filename="", overwrite=FALSE, wopt=list())  {
 		}
 		if (length(spreds) > 0) {
 			v <- lapply(spreds, function(r) {
-				as.vector(t(readValues(r, b$row[i], b$nrows[i], 1, nc, TRUE)))
+				as.vector(t(terra::readValues(r, b$row[i], b$nrows[i], 1, nc, TRUE)))
 			})
 			vv <- do.call(cbind, v)
 			names(vv) <- nms[!di]
@@ -53,10 +53,10 @@ function(x, ..., filename="", overwrite=FALSE, wopt=list())  {
 		}
 		
 		eco <- run(x)
-		writeValues(out, eco, c(b$row[i], b$nrows[i]))
+		terra::writeValues(out, eco, c(b$row[i], b$nrows[i]))
 	}
-	readStop(x)
-	out <- writeStop(out)
+	terra::readStop(x)
+	out <- terra::writeStop(out)
 	return(out)
 }
 )
