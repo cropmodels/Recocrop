5be51f1350196c48fe8ffee35b2e6b4150400628
Robert+1-at-925791393128 <r.hijmans@gmail.com>
2020-04-17 19:59:10 -0700

    plot

--- R/ecocrop.R
+++ R/ecocrop.R
@@ -139,35 +139,3 @@ setMethod ("show" , "Rcpp_EcocropModel",
 	}
 )	
 
-
-setMethod ("plot" , "Rcpp_EcocropModel", 
-	function(x, ...) {
-		pars <- x$parameters
-		nms <- x$parameter_names
-		n <- length(pars)
-		nc <- ceiling(sqrt(n))
-		nr <- ceiling(n / nc)
-
-		old.par <- graphics::par(no.readonly = TRUE) 
-		on.exit(graphics::par(old.par))
-		graphics::par(mfrow=c(nr, nc), mar=c(2, 3, 2, 1))
-
-		for (i in 1:n) {
-			p <- pars[[i]]
-			if (!is.finite(p[2])) {
-				p[1] <- p[2] <- p[3] - 2*(p[4]-p[3])
-			} else if (!is.finite(p[3])) {
-				p[4] <- p[3] <- p[2] + 2*(p[2]-p[1])
-			}
-			d <- (p[4] - p[1]) / 4
-			p <- c(p[1]-d, p, p[4]+d)
-			xy <- cbind(p, c(0,0,1,1,0,0))
-			plot(xy, ylab="suitability", las=1, type="l", lwd=2, col="red", axes=FALSE)
-			points(xy[2:5,])
-			graphics::axis(1, pos=-0.05)
-			graphics::axis(2, las=2)
-			graphics::title(nms[i])
-		}
-	}
-)	
-
